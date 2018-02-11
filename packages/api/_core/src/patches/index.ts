4c27b64427a3af59b0940cb17df179513ea94314
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-02-10 22:24:37 -0500

    clean up some unneeded patches

--- packages/api/_core/src/patches/index.ts
+++ packages/api/_core/src/patches/index.ts
@@ -1,13 +1,9 @@
 import {patchGetSpecs} from './getSpecs';
 import {patchStartNew} from './startNew';
-import {patchRestartKernel} from './restartKernel';
-import {patchDispose} from './dispose';
 import {patchListRunning} from './listRunning';
 
 export const patches = {
   patchGetSpecs,
   patchStartNew,
-  patchRestartKernel,
-  patchDispose,
   patchListRunning,
 };

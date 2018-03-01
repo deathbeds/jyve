d7bc424aaaaae7ec8a95ef63689302a26e60ec75
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-03-01 16:33:20 -0500

    more renaming

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,6 +2,6 @@
 . ./scripts/env.sh
 
 echo "Watching jyve files..."
-while inotifywait -q -r -e modify,create,delete $PACKAGES/*/*/{lib,style}; do
+while inotifywait -q -r -e modify,create,delete $PACKAGES/*/{lib,style}; do
   . ./scripts/sync.sh
 done

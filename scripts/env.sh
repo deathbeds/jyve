ea8c7dff9103356dfb574246d87419fba4b94a19
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-05-31 01:33:35 -0400

    delete more things

--- scripts/env.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export DEATHBEDS=$CONDA_PREFIX/share/jupyter/lab/staging/node_modules/@deathbeds
-export PACKAGES=./packages
--- scripts/geckodriver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-wget --continue https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
-tar -xaf geckodriver-v0.20.0-linux64.tar.gz -C $CONDA_PREFIX/bin

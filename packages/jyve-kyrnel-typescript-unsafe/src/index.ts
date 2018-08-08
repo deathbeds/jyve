03abbde43309f055b4bbd7ace0bd7a79ba1ca3c6
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-08-07 22:05:29 -0400

    actually make typescript load async

--- packages/jyve-kyrnel-typescript-unsafe/src/index.ts
+++ packages/jyve-kyrnel-typescript-unsafe/src/index.ts
@@ -1,6 +1,11 @@
+// tslint:disable-next-line
+/// <reference path="../../../node_modules/@types/webpack-env/index.d.ts"/>
+
 import {Kernel} from '@jupyterlab/services';
 import {Jyve} from '@deathbeds/jyve';
 import {JyveKernel} from '@deathbeds/jyve/lib/kernel';
+
+import * as KernelModuleType from './kernel';
 
 // tslint:disable-next-line
 export const {jyve} = require('../package.json') as any;
@@ -11,6 +16,19 @@ export async function newKernel(
   options: JyveKernel.IOptions,
   id: string
 ): Promise<Jyve.IJyveKernel> {
-  const kernel = await import('./kernel');
+  let kernel = await new Promise<typeof KernelModuleType>((resolve, reject) => {
+    require.ensure(
+      ['./kernel'],
+      (require) => {
+        const kernel = require('./kernel') as typeof KernelModuleType;
+        resolve(kernel);
+      },
+      (error: any) => {
+        console.error(error);
+        reject();
+      },
+      'typescript'
+    );
+  });
   return new kernel.TypeScriptUnsafeKernel(options, id);
 }

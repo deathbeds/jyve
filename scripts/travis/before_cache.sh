c3f92ac4f2ee1875ba3abaf3e7e43689d1b1ffbc
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-08-07 21:11:56 -0400

    remove cache

--- scripts/travis/before_cache.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-rsync -av ${CONDA_DIR}/pkgs/*.tar.bz2 ${HOME}/.cache/conda/
--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -3,12 +3,7 @@ set -ex
 
 conda env update -n jyve-dev --file ${TRAVIS_BUILD_DIR}/environment.yml
 
-# TODO: get geckodriver from conda-forge
-tar -xaf ${HOME}/.cache/stuff/${GECKODRIVER_DIST} \
-  -C ${CONDA_DIR}/envs/jyve-dev/bin
-
 source activate jyve-dev
-geckodriver --version
 
 cd ${TRAVIS_BUILD_DIR}
 

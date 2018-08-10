0ff3d1e464f41f690ba6c6cd968892b6b36eda3a
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-08-09 20:53:33 -0400

    add pyodide submodule, serverextension

--- jyve/__init__.py
+++ jyve/__init__.py
@@ -1,1 +1,10 @@
 from ._version import __version__  # noqa
+
+from .serverextension import JyveServerExtension
+
+
+def load_jupyter_server_extension(nb_server_app):
+    (JyveServerExtension(nb_server_app)
+     .patch_wasm()
+     .patch_nbconvert()
+     .warn("jyve activated"))
---
+++ jyve/serverextension.py
@@ -0,0 +1,49 @@
+import mimetypes
+
+from traitlets.config import LoggingConfigurable
+from nbconvert.exporters.export import exporter_map
+
+from .exporter import JyveExporter
+
+
+WASM_MIME = "application/wasm"
+EMO = "🗂"
+
+
+class JyveServerExtension(LoggingConfigurable):
+    def __init__(self, notebookapp):
+        super().__init__()
+        self._app = notebookapp
+        self.log = notebookapp.log
+
+    def error(self, msg, *args):
+        self.log.error(f"{EMO}      {msg}", *args)
+        return self
+
+    def warn(self, msg, *args):
+        self.log.debug(f" {EMO}     {msg}", *args)
+        return self
+
+    def info(self, msg, *args):
+        self.log.info(f"  {EMO}    {msg}", *args)
+        return self
+
+    def patch_wasm(self):
+        _guess = mimetypes.guess_type
+
+        def guess_type(name, strict=True):
+            # https://www.iana.org/assignments/provisional-standard-media-types/provisional-standard-media-types.xhtml
+            if name[-5:] == ".wasm":
+                self.warn(f"Saying {name} is some wasm")
+                return (WASM_MIME, None)
+            return _guess(name, strict)
+
+        mimetypes.guess_type = guess_type
+
+        return self.warn(f"mimetypes is no longer safe")
+
+    def patch_nbconvert(self):
+        exporter_map.update(
+            jyve=JyveExporter,
+        )
+        return self.warn(f"nbconvert is no longer safe")
